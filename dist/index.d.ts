80084d5644ad3de351835129410abd5dbedf0fd4
Johan PIQUET <johan.piquet@gmail.com>
2025-08-24 22:28:02 +0200

    Merging broken commits.

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import "jopi-node-space";
+// Allow initializing his core and stop some difficulties with import orders.
+import "jopi-rewrite";
---
+++ dist/index.js
@@ -0,0 +1,19 @@
+import * as NodeModule from 'node:module';
+import "jopi-node-space";
+// Allow initializing his core and stop some difficulties with import orders.
+import "jopi-rewrite";
+import { installBunJsLoader } from "@jopi-loader/tools";
+// Guard to avoid recursive self-registration when using Module.register(import.meta.url)
+const __JOPI_LOADER_REGISTERED__ = Symbol.for('jopi-loader:registered');
+const __g = globalThis;
+if (!__g[__JOPI_LOADER_REGISTERED__]) {
+    __g[__JOPI_LOADER_REGISTERED__] = true;
+    if (NodeSpace.what.isNodeJS) {
+        // "register" allow async.
+        NodeModule.register(new URL('./nodeJsLoader.js', import.meta.url));
+    }
+    else if (NodeSpace.what.isBunJs) {
+        installBunJsLoader();
+    }
+}
+//# sourceMappingURL=index.js.map
