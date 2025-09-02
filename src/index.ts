37798d79a6cf3d542b8606feaf1482c6677f2195
Johan PIQUET <johan.piquet@gmail.com>
2025-09-02 19:44:27 +0200

    Removing jopi-rewrite ref

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import * as NodeModule from 'node:module';
 
 import "jopi-node-space";
-
-// Allow initializing his core and stop some difficulties with import orders.
-import "jopi-rewrite";
 import {installBunJsLoader} from "@jopi-loader/tools";
 
 // Guard to avoid recursive self-registration when using Module.register(import.meta.url)
