8ce06e6ff9f31e99d0ce93c02d6647b6e7c7255d
Johan PIQUET <johan.piquet@gmail.com>
2025-10-14 11:10:37 +0200

    Refactoring NodeSpace

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as NodeModule from 'node:module';
 
-import NodeSpace from "jopi-node-space";
+import {isNodeJS, isBunJS} from "jopi-node-space/ns_what";
 import {installBunJsLoader} from "@jopi-loader/tools";
 
 // Guard to avoid recursive self-registration when using Module.register(import.meta.url)
@@ -10,10 +10,10 @@ const __g: any = globalThis as any;
 if (!__g[__JOPI_LOADER_REGISTERED__]) {
     __g[__JOPI_LOADER_REGISTERED__] = true;
 
-    if (NodeSpace.what.isNodeJS) {
+    if (isNodeJS) {
         // "register" allow async.
         NodeModule.register(new URL('./nodeJsLoader.js', import.meta.url));
-    } else if (NodeSpace.what.isBunJs) {
+    } else if (isBunJS) {
         installBunJsLoader();
     }
 }
